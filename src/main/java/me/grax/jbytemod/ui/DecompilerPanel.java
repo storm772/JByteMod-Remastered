af9b511f05397046d62fea29f7a3be612afbcc4c
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-02-18 16:27:47 +0100

    Fix decompiler panel if using light theme

--- src/main/java/me/grax/jbytemod/ui/DecompilerPanel.java
+++ src/main/java/me/grax/jbytemod/ui/DecompilerPanel.java
@@ -1,5 +1,6 @@
 package me.grax.jbytemod.ui;
 
+import me.grax.jbytemod.JByteMod;
 import me.grax.jbytemod.utils.ErrorDisplay;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.Theme;
@@ -15,9 +16,14 @@ public class DecompilerPanel extends RSyntaxTextArea {
         this.setAntiAliasingEnabled(true);
         this.setFont(new Font("Monospaced", Font.PLAIN, 12));
         this.setEditable(false);
-        //change theme to java
+        // change theme to java
         try {
-            Theme theme = Theme.load(getClass().getResourceAsStream("/org/fife/ui/rsyntaxtextarea/themes/dark.xml"));
+            Theme theme;
+            if (!JByteMod.ops.get("use_flatdark").getBoolean()) {
+                theme = Theme.load(getClass().getResourceAsStream("/org/fife/ui/rsyntaxtextarea/themes/idea.xml"));
+            } else {
+                theme = Theme.load(getClass().getResourceAsStream("/org/fife/ui/rsyntaxtextarea/themes/dark.xml"));
+            }
             theme.apply(this);
         } catch (IOException e1) {
             new ErrorDisplay(e1);
